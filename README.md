d3deae5b25a56d3d12db87d1636a7dc68b27c144
hanlee <me@hanlee.co>
2020-04-04 03:58:58 +0900

    done

---
+++ README.md
@@ -0,0 +1,15 @@
+# Image to Url Bot
+
+Telegram bot auto generate url by send photo
+
+## Run it
+
+Before run it change `template.env` to `.env` then set your own token.
+
+```bash
+# With nodemon
+npm run dev
+
+# With node
+npm run start
+```
---
+++ bot.js
@@ -0,0 +1,19 @@
+const Telegraf = require('telegraf');
+require('dotenv').config();
+
+const bot = new Telegraf(process.env.API_TOKEN);
+
+const savePhoto = async function (ctx) {
+  const photoId = ctx.message.photo[2].file_id;
+  const url = await ctx.telegram.getFileLink(photoId);
+  console.log(url);
+  return ctx.reply(url);
+};
+
+bot.start((ctx) => ctx.reply('Welcome! this bot will change image to url!'));
+bot.help((ctx) =>
+  ctx.reply('Just send a photo than you will get the url link to this photo.')
+);
+bot.on('photo', savePhoto);
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.launch();
