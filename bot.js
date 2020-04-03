e51831c45144ddffd87e8114e3e4e2ece35510c6
hanlee <me@hanlee.co>
2020-04-04 04:08:02 +0900

    change to get last in photoId

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const bot = new Telegraf(process.env.API_TOKEN);
 
 const savePhoto = async function (ctx) {
-  const photoId = ctx.message.photo[2].file_id;
+  const photoId = ctx.message.photo[ctx.message.photo.length - 1].file_id;
   const url = await ctx.telegram.getFileLink(photoId);
   console.log(url);
   return ctx.reply(url);
